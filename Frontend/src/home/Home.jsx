dc043c785fd32624983f2c73e6c7b5acb0c57b8c
Suyog Mohite <suyogmohite0803@gmail.com>
2025-12-09 12:06:03 +0530

    add

--- Frontend/src/home/Home.jsx
+++ Frontend/src/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
-import FreeBook from '../components/FreeBook'
+import FreeBook from '../components/Freebook'
 import Footer from '../components/Footer'
 
 function Home() {
